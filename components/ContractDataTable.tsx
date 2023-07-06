f2a530a98196680e4fad04aebaad6ef0e2fe4950
codyrhoten <rhoten_cody@yahoo.com>
2023-07-06 09:08:20 -0400

    Trim contracts addresses in Data Table & add Mantine tool tips

--- components/ContractDataTable.tsx
+++ components/ContractDataTable.tsx
@@ -1,4 +1,4 @@
-import { /* useMantineTheme, */ createStyles, Text, Table, Badge } from "@mantine/core";
+import { /* useMantineTheme, */ createStyles, Table, Badge, Tooltip } from "@mantine/core";
 
 type ContractData = {
     address: string;
@@ -24,6 +24,12 @@ const useStyles = createStyles((theme) => ({
     },
 }));
 
+function shortenAddress(address: string): string {
+    let start = address.substring(0, 6);
+    let end = address.substring(address.length - 4);
+    return start + "..." + end;
+};
+
 export default function ContractDataTable({ contractAddresses }: ContractDataTableProps) {
     // const theme = useMantineTheme();
     const { classes } = useStyles();
@@ -38,19 +44,27 @@ export default function ContractDataTable({ contractAddresses }: ContractDataTab
             <Table>
                 <thead>
                     <tr>
-                        <th style={{textAlign: "center"}}>Contract Address</th>
-                        <th style={{textAlign: "center"}}>Contract Deployer</th>
-                        <th style={{textAlign: "center"}}>Deployed Block Number</th>
-                        <th style={{textAlign: "center"}}>Name</th>
-                        <th style={{textAlign: "center"}}>Symbol</th>
-                        <th style={{textAlign: "center"}}>Total Balance</th>
+                        <th style={{ textAlign: "center" }}>Contract Address</th>
+                        <th style={{ textAlign: "center" }}>Contract Deployer</th>
+                        <th style={{ textAlign: "center" }}>Deployed Block Number</th>
+                        <th style={{ textAlign: "center" }}>Name</th>
+                        <th style={{ textAlign: "center" }}>Symbol</th>
+                        <th style={{ textAlign: "center" }}>Total Balance</th>
                     </tr>
                 </thead>
                 <tbody>
                     {contractAddresses.map((contractData, i) => (
                         <tr key={i}>
-                            <td>{contractData.address}</td>
-                            <td>{contractData.contractDeployer}</td>
+                            <td>
+                                <Tooltip label={contractData.address}>
+                                    <span>{shortenAddress(contractData.address)}</span>
+                                </Tooltip>
+                            </td>
+                            <td>
+                                <Tooltip label={contractData.contractDeployer}>
+                                    <span>{shortenAddress(contractData.contractDeployer)}</span>
+                                </Tooltip>
+                            </td>
                             <td>{contractData.deployedBlockNumber}</td>
                             <td>{contractData.name}</td>
                             <td>
