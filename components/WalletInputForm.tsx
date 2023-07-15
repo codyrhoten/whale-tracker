399226e73538c4abb3579a43b08165b3053e3382
codyrhoten <rhoten_cody@yahoo.com>
2023-07-15 11:37:19 -0400

    Change API call and pass props from index.tsx

--- components/WalletInputForm.tsx
+++ components/WalletInputForm.tsx
@@ -5,123 +5,135 @@ import { useForm } from "@mantine/form";
 import axios from "axios";
 
 const useStyles = createStyles((theme) => ({
-  input: {
-    marginTop: theme.spacing.md,
-    marginBottom: "1.5rem",
-  },
-  error: {
-    color: theme.colors.red[7],
-  },
+    input: {
+        marginTop: theme.spacing.md,
+        marginBottom: "1.5rem",
+    },
+    error: {
+        color: theme.colors.red[7],
+    },
 }));
 
-interface FormProps {
-  onSubmit: (address: string) => void;
-}
+export default function WalletInputForm({ setContractData, address, error, setAddress, setError }: {
+    address: string;
+    setAddress: (address: string) => void;
+    error: string;
+    setError: (error: string) => void;
+    setContractData: (error: string) => void;
+}) {
 
-export default function WalletInputForm({ onSubmit }: FormProps) {
-  const [address, setAddress] = useState("");
-  const [error, setError] = useState("");
+    console.log('address', address);
 
-  const isValidENSAddress = async (address: string) => {
-    try {
-      const provider = new ethers.providers.JsonRpcProvider();
+    const alchemyApiKey = process.env.NEXT_PUBLIC_ALCHEMY_KEY;
+    const provider = new ethers.providers.AlchemyProvider("mainnet", alchemyApiKey);
 
-      const resolvedAddress = await provider.resolveName(address);
-      if (!resolvedAddress) return false;
-      return ethers.utils.isAddress(resolvedAddress);
-    } catch (error) {
-      console.error("Error validating ENS address:", error);
-      return false;
-    }
-  };
+    const isValidENSAddress = async (address: string) => {
+        try {
 
-  const validateWalletAddress = async (address: string): Promise<boolean> => {
-    let isValidAddress = false;
-
-    if (address.startsWith("0x")) {
-      const isValid = ethers.utils.isAddress(address);
-
-      if (isValid) isValidAddress = true;
-    }
-
-    if (!address.startsWith("0x")) {
-      const isValid = await isValidENSAddress(address);
-
-      if (isValid) isValidAddress = true;
-    }
-
-    return isValidAddress;
-  };
-
-  const form = useForm({
-    initialValues: {
-      address: "",
-    },
-    validate: {
-      address: async (value) => {
-        const isValid = await validateWalletAddress(value);
-        if (!isValid) {
-          return "Invalid Ethereum address";
+            const resolvedAddress = await provider.resolveName(address);
+            if (!resolvedAddress) return false;
+            return ethers.utils.isAddress(resolvedAddress);
+        } catch (error) {
+            console.error("Error validating ENS address:", error);
+            return false;
         }
-      },
-    },
-  });
+    };
 
-  const { errors, reset, isValid, setValues } = form;
+    const validateWalletAddress = async (address: string): Promise<boolean> => {
+        let isValidAddress = false;
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
+        if (address.startsWith("0x")) {
+            const isValid = ethers.utils.isAddress(address);
 
-    if (!address) {
-      setError("Please enter an Ethereum address");
-      return;
-    }
+            if (isValid) isValidAddress = true;
+        }
 
-    if (!isValid()) {
-      setError(errors.address?.toString() || "Invalid Ethereum address.");
-      return;
-    }
+        if (!address.startsWith("0x")) {
+            const isValid = await isValidENSAddress(address);
 
-    try {
-      const response = await axios.get("/api/get-contract-for-owner", {
-        params: { owner: address },
-      });
-      const data = response.data;
+            if (isValid) isValidAddress = true;
+        }
 
-      if (data.error) {
-        setError(data.error);
-      } else {
-        onSubmit(address);
-        reset();
+        return isValidAddress;
+    };
+
+    const form = useForm({
+        initialValues: {
+            address: "",
+        },
+        validate: {
+            address: async (value) => {
+                const isValid = await validateWalletAddress(value);
+                if (!isValid) {
+                    return "Invalid Ethereum address";
+                }
+            },
+        },
+    });
+
+    const { errors, reset, isValid, setValues } = form;
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        console.log('handleSubmit');
+
+        /* if (!address) {
+            setError("Please enter an Ethereum address");
+            return;
+        }
+
+        if (!isValid()) {
+            setError(errors.address?.toString() || "Invalid Ethereum address.");
+            return;
+        } */
+
+        console.log('1');
+
+        try {
+            const response = await fetch("/api/get-contract-for-owner", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ address }),
+            });
+            const data = await response.json();
+            console.log('data', data)
+
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setContractData(data.contracts);
+                reset();
+                setError("");
+            }
+        } catch (err) {
+            setError("An error occurred.");
+        }
+    };
+
+    const handleAddressChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setAddress(e.target.value);
         setError("");
-      }
-    } catch (err) {
-      setError("An error occurred.");
-    }
-  };
+        setValues({ address: e.target.value });
+    };
 
-  const handleAddressChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setAddress(e.target.value);
-    setError("");
-    setValues({ address: e.target.value });
-  };
+    const { classes } = useStyles();
 
-  const { classes } = useStyles();
-
-  return (
-    <Container>
-      <form onSubmit={handleSubmit}>
-        {error && <div className={classes.error}>{error}</div>}
-        <TextInput
-          value={address}
-          onChange={handleAddressChange}
-          placeholder="Enter Ethereum address"
-          className={classes.input}
-        />
-        <Button type="submit" variant="outline" fullWidth>
-          Submit
-        </Button>
-      </form>
-    </Container>
-  );
+    return (
+        <Container>
+            <form onSubmit={handleSubmit}>
+                {error && <div className={classes.error}>{error}</div>}
+                <TextInput
+                    value={address}
+                    onChange={handleAddressChange}
+                    placeholder="Enter Ethereum address"
+                    className={classes.input}
+                />
+                <Button type="submit" variant="outline" fullWidth>
+                    Submit
+                </Button>
+            </form>
+        </Container>
+    );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,60 @@
 import { useEffect, useState } from "react";
 import ContractDataTable from "@/components/ContractDataTable";
-import { WalletInputForm } from "@/components/WalletInputForm";
+import WalletInputForm from "@/components/WalletInputForm";
 
 type ContractData = {
-  address: string;
-  contractDeployer: string;
-  deployedBlockNumber: number;
-  name: string;
-  symbol: string;
-  totalBalance: number;
+    address: string;
+    contractDeployer: string;
+    deployedBlockNumber: number;
+    name: string;
+    symbol: string;
+    totalBalance: number;
 };
 
 export default function Home() {
-  const [contractData, setContractData] = useState<ContractData[]>([]);
+    const [contractData, setContractData] = useState<ContractData[]>([]);
+    const [address, setAddress] = useState("");
+    const [error, setError] = useState("");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/get-contract-for-owner", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ address: "vitalik.eth" }),
-        });
-        const data = await response.json();
-        console.log("contract data", data);
-        setContractData(data.contracts);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
+    const handleSetContractData = (data: any) => {
+        console.log(data);
+
+        setContractData(data);
     };
+    // const [tableData, setTableData] = useState({});
 
-    fetchData(); // Call the async function to initiate the fetch request
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch("/api/get-contract-for-owner", {
+    //                 method: "POST",
+    //                 headers: {
+    //                     "Content-Type": "application/json",
+    //                 },
+    //                 body: JSON.stringify({ address: "vitalik.eth" }),
+    //             });
+    //             const data = await response.json();
+    //             console.log("contract data", data);
+    //             setContractData(data.contracts);
+    //         } catch (error) {
+    //             console.error("Error fetching data:", error);
+    //         }
+    //     };
 
-    // Cleanup function (optional)
-    return () => {
-      // Perform any necessary cleanup here (e.g., cancel any pending requests)
-    };
-  }, []);
+    //     fetchData(); // Call the async function to initiate the fetch request
 
-  return (
-    <>
-      <ContractDataTable contractAddresses={contractData} />
-    </>
-  );
+    //     // Cleanup function (optional)
+    //     return () => {
+    //         // Perform any necessary cleanup here (e.g., cancel any pending requests)
+    //     };
+    // }, []);
+
+    return (
+        <>
+            <WalletInputForm setContractData={handleSetContractData} address={address} error={error} setAddress={setAddress} setError={setError} />
+            {Object.values(contractData).length && (
+                <ContractDataTable contractAddresses={contractData} />
+            )}
+        </>
+    );
 }
